08d8908cda74f4e618e5c42e71caf6654350f304
Reeve Hou <sam2972570@gmail.com>
2024-02-26 16:28:04 +0800

    Add dtype into pipline to judge which data we are crawling.

--- fin_database/main.py
+++ fin_database/main.py
@@ -5,8 +5,10 @@ from fin_database.pipeline import Pipeline
 
 def main():
     date_start = date(2020, 2, 14)
-    date_end = date(2020, 2, 21)
-    Pipeline().produce(date_start, date_end)
+    date_end = date(2020, 2, 27)
+    dtype = 'daily'
+    # dtype = 'month'
+    Pipeline().produce(date_start, date_end, dtype)
 
 
 if __name__ == '__main__':
--- fin_database/pipeline.py
+++ fin_database/pipeline.py
@@ -8,17 +8,22 @@ from fin_database.steps.storer import Storer
 
 
 class Pipeline:
-    def produce(self, date_start, date_end):
+    def produce(self, date_start, date_end, dtype):
         utils = Utils()
-        result = PreCheck().daily_check(date_start, date_end, utils)
-        if result['keep_run'] == True:
-            steps = [Crawler(), Parser(), Storer()]
-            for date_ in result['date_list']:
-                input_ = {'date': date_, 'conn': result['conn'], 'c': result['c'], 'keep_run': True}
-                for step in steps:
-                    if input_['keep_run'] == False:
-                        break
-                    input_ = step.daily_process(input_, utils)
+        match dtype:
+            case 'daily':
+                result = PreCheck().daily_check(date_start, date_end, utils)
+                if result['keep_run'] == True:
+                    steps = [Crawler(), Parser(), Storer()]
+                    for date_ in result['date_list']:
+                        input_ = {'date': date_, 'conn': result['conn'], 'c': result['c'], 'keep_run': True}
+                        for step in steps:
+                            if input_['keep_run'] == False:
+                                break
+                            input_ = step.daily_process(input_, utils)
 
-                sleep(5)
-            result['conn'].close()
+                        sleep(5)
+                    result['conn'].close()
+
+            case 'month':
+                pass
